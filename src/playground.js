72d30f2bef7a47dcb20247d1a5e8b00c7cd4ebdc
Louis Grellet <louis.grellet@gmail.com>
2019-09-21 19:06:20 +0200

    test

--- src/playground.js
+++ src/playground.js
@@ -4,7 +4,7 @@ import MyComponent from './index'
 
 export default (props) => {
   return (<div className="m-auto col-6 p-5 text-center">
-    <small>[ Playground ]</small>
+    <small>[ Playground - loady ]</small>
     <MyComponent />
   </div>)
 }
